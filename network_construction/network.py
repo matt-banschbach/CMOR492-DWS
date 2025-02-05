7df28ddca5be1105cddab870bc92892fe5135da8
matt-banschbach <mbanschbach@protonmail.ch>
2025-02-05 13:13:23 -0600

    network creation works with other cities

--- network_construction/network.py
+++ network_construction/network.py
@@ -6,20 +6,21 @@ from configparser import ConfigParser
 import numpy as np
 import random
 
-def get_road_net(place):
+def get_road_net(place, file_name):
 
     config = ConfigParser()
     config.read('C:\\Users\mbans\Desktop\CMOR492-DWS\DWS\config.ini')
     googlemaps_key = config.get('api_keys', 'googlemaps')
 
-    place_name = "Uniontown, Alabama, USA"
-    G = ox.graph_from_place(place_name, network_type='drive')
+    # place_name = "Uniontown, Alabama, USA"
+    G = ox.graph_from_place(place, network_type='drive')
     G = ox.elevation.add_node_elevations_google(G, api_key=googlemaps_key)
 
     for u, v, data in G.edges(data=True):
         data.pop('geometry', None)  # Remove the attribute if it exists
 
-    ox.save_graphml(G, 'road_net_2.graphml')
+    # ox.save_graphml(G, 'road_net_2.graphml')
+    ox.save_graphml(G, file_name)
 
     return G
 
